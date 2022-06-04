17a3306ddab6f9c91075dfc5435816ee040a4b98
Kalpana Kondapalli <projectsmycreator@gmail.com>
2022-06-04 20:09:20 +0530

    Initial TestNG Maven commit

---
+++ src/test/java/com/example/NavigationTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class NavigationTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Test method");
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
